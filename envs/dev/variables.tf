f228852c715554b733d43a55ea66fdef5a2c4928
vcthriee <vcthrieearikpo@gmail.com>
2026-03-17 15:18:09 +0100

    complete infra with ecs

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -35,6 +35,24 @@ variable "private_app_cidrs" {
 variable "private_data_cidrs" {
   type    = list(string)
   default = ["10.0.5.0/24", "10.0.6.0/24"]
+} 
+
+variable "db_name" {
+  type    = string
+  default = "ecommerce"
 }
 
+variable "db_username" {
+  type    = string
+  default = "dbadmin"
+}
 
+variable "db_password" {
+  type    = string
+  default = "changeme123"
+}
+
+variable "jwt_secret" {
+  type    = string
+  default = "dev-secret-change-in-production"
+}
